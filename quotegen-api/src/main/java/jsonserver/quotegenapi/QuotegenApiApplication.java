260e8147a6956aa888eae7420fedf1909808b4ac
nurikcat <221564@astanait.edu.kz>
2023-12-23 19:31:44 +0600

    build: the necessary dependencies has been added

---
+++ quotegen-api/src/main/java/jsonserver/quotegenapi/QuotegenApiApplication.java
@@ -0,0 +1,13 @@
+package jsonserver.quotegenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotegenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotegenApiApplication.class, args);
+	}
+
+}
---
+++ quotegen-api/src/test/java/jsonserver/quotegenapi/QuotegenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jsonserver.quotegenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotegenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
